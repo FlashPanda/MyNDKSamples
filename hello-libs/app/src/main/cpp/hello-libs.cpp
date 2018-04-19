50189a77976f722951a235367a44ac7b952025ec
FlashPanda <caizhoudong001@gmail.com>
2018-04-19 13:14:09 +0800

    添加项目代码

---
+++ hello-libs/app/src/main/cpp/hello-libs.cpp
@@ -0,0 +1,26 @@
+#include <cstring>
+#include <jni.h>
+#include <cinttypes>
+#include <android/log.h>
+// 这两个是自己的头文件
+#include <gmath.h>
+#include <gperf.h>
+
+#define LOGI(...) \
+	((void)__android_log_print)(ANDROID_LOG_INFO, "hello-libs::", __VA_ARGS__))
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_hellolibs_MainActivity_stringFronJNI (JNIEnv* env, jobject thiz) {
+	// 越简单越好
+	auto ticks = GetTicks();
+
+	for (auto exp = ; exp < 32; ++exp) {
+		volatile unsigned val = gpower (exp);
+		(void) val;
+	}
+	ticks = GetTicks() - ticks;
+
+	LOGI("calculation time: %" PRIu64, ticks);
+
+	return env->NewStringUTF("Hello from JNI LIBS!");
+}
--- hello-libs/app/src/main/java/com/example/czd/hello_libs/MainActivity.java
+++ hello-libs/app/src/main/java/com/example/czd/hello_libs/MainActivity.java
@@ -5,12 +5,6 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-	// Used to load the 'native-lib' library on application startup.
-	static {
-		System.loadLibrary("native-lib");
-	}
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,9 +15,8 @@ public class MainActivity extends AppCompatActivity {
 		tv.setText(stringFromJNI());
 	}
 
-	/**
-	 * A native method that is implemented by the 'native-lib' native library,
-	 * which is packaged with this application.
-	 */
 	public native String stringFromJNI();
+	static {
+		System.loadLibrary("hello-libs");
+	}
 }
