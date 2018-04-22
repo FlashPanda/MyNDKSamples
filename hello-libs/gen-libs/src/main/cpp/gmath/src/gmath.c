1141fca5763ae83fdcf95ebb36cabb4259dc0585
JasonCai <caizhoudong@126.com>
2018-04-22 08:47:49 +0800

    实现函数

--- hello-libs/gen-libs/src/main/cpp/gmath/src/gmath.c
+++ hello-libs/gen-libs/src/main/cpp/gmath/src/gmath.c
@@ -1,9 +1,30 @@
 //
-// Created by Administrator on 2018/4/19.
+// Created by Administrator on 2018/4/21.
 //
 
-#include <android/log.h>
 #include "gmath.h"
+#include <android/log.h>
 
 #if defined(__GNUC__) && __GNUC__ >= 4
-#define GMATH_EXPORT __attribute__
+#define GAMATH_EXPORT __attribute__((visibility("default")))
+#elif defined(__SUNPRO_C) && (__SUNPRO_C >= 0x590)
+#define GMATH_EXPORT __attribute__((visibility("default")))
+#else
+#define GMATH_EXPORT
+#endif
+
+#define LOGE(...)\
+	((void)__android_log_print(ANDROID_LOG_ERROR, "gmath::", __VA_ARGS__))
+
+GAMTH_EXPORT unsigned gpower(unsigned n){
+	if (n == 0)
+		return 1;
+	if (n > 31) {
+		LOGE("error from power(%d): integer overflow", n);
+		return 0;
+	}
+	unsigned val = gpower (n >> 1) * gpower (n >> 1);
+	if (n & 1)
+		val *= 2;
+	return val;
+}
--- hello-libs/gen-libs/src/main/cpp/gmath/src/gmath.h
+++ hello-libs/gen-libs/src/main/cpp/gmath/src/gmath.h
@@ -1,13 +1,13 @@
 //
-// Created by Administrator on 2018/4/19.
+// Created by Administrator on 2018/4/21.
 //
 
 #ifndef HELLO_LIBS_GMATH_H
 #define HELLO_LIBS_GMATH_H
 
-// 返回 pow(2, n)
 #ifdef __cplusplus
 extern "C"
 #endif
 	unsigned gpower(unsigned n);
+
 #endif //HELLO_LIBS_GMATH_H
